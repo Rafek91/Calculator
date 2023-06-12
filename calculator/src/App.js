c84c6319d9d7c5240b4ec92d73a6212fdd429e89
Rafek91 <radek.chlud@gmail.com>
2023-06-12 14:41:30 +0200

    update 12.6.2023 - 14:41

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,67 @@
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [input,setInput] = useState('')
+  const doubleOperatorCheck = (character) => {
+    const doubleOperators = ['+','-','/','*','.']
+    const lastInputCharacter = input.split('').pop()
+    console.log(lastInputCharacter)
+
+    const checkCondition = doubleOperators.includes(lastInputCharacter) && doubleOperators.includes(character)
+    if(!checkCondition){
+      setInput(input+character)
+    }
+}
+
+  const calculationFunction = (inputString) => {
+    const doubleOperators = ['+','-','/','*','.']
+    const lastInputCharacter = input?.split('').pop()
+    const checkCondition = doubleOperators.includes(lastInputCharacter)
+
+    if(!checkCondition){
+      let result = Function(`return ${inputString}`)()
+      setInput(`${result}`)
+    }
+  }
+
   return (
-    <>
-    <p>nic</p>
-    </>
+    <div id="table">
+      <table id="calculator">
+        <tbody>
+        <tr>
+          <td colSpan="3" id="calc-result">{input}</td>
+          <td onClick={()=>{setInput('')}}id="calc-clear">c</td>
+        </tr>
+        <tr>
+          <td onClick={()=>{doubleOperatorCheck(1)}}>1</td>
+          <td onClick={()=>{doubleOperatorCheck(2)}}>2</td>
+          <td onClick={()=>{doubleOperatorCheck(3)}}>3</td>
+          <td onClick={()=>{doubleOperatorCheck('+')}}>+</td>
+        </tr>
+        <tr>
+          <td onClick={()=>{doubleOperatorCheck(4)}}>4</td>
+          <td onClick={()=>{doubleOperatorCheck(5)}}>5</td>
+          <td onClick={()=>{doubleOperatorCheck(6)}}>6</td>
+          <td onClick={()=>{doubleOperatorCheck('-')}}>-</td>
+        </tr>
+        <tr>
+          <td onClick={()=>{doubleOperatorCheck(7)}}>7</td>
+          <td onClick={()=>{doubleOperatorCheck(8)}}>8</td>
+          <td onClick={()=>{doubleOperatorCheck(9)}}>9</td>
+          <td onClick={()=>{doubleOperatorCheck('*')}}>*</td>
+        </tr>
+        <tr>
+          <td onClick={()=>{doubleOperatorCheck(0)}}>0</td>
+          <td onClick={()=>{doubleOperatorCheck('.')}}>.</td>
+          <td onClick={()=>{calculationFunction(input)}}>=</td>
+          <td onClick={()=>{doubleOperatorCheck('/')}}>/</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
 
--- calculator/src/index.js
+++ calculator/src/index.js
@@ -5,7 +5,5 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
