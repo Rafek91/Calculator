1bf594f591be30e146d11a61456a01d31d774f00
Rafek91 <95579080+Rafek91@users.noreply.github.com>
2023-10-30 17:41:00 +0100

    Update App.js

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,31 +1,10 @@
 import './App.css';
 import { useState } from 'react';
-import History from './History';
 
 const doubleOperators = ['+','-','/','*','.']
-const databseURL = "http://localhost:3500/history"
 
 function App() {
   const [input,setInput] = useState('')
-
-  const addHistoryRecord = async (newRecord) => {
-    try {
-      const response = await fetch(databseURL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newRecord)
-      });
-      if (response.ok) {
-        console.log('Ok');
-      } else {
-        console.error('Failed');
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
 
   const doubleOperatorCheck = (character) => {
     const inputCopy = input.slice("")
@@ -43,9 +22,6 @@ function App() {
     if(!checkCondition){
       let result = Function(`return ${inputString}`)()
       setInput(`${result}`);
-
-      const newHistoryRecord = {value:`${inputString}=${result}`}
-      addHistoryRecord(newHistoryRecord);
     }
   }
 
@@ -80,18 +56,12 @@ function App() {
               <tr>
                 <td onClick={()=>{doubleOperatorCheck(0)}}>0</td>
                 <td onClick={()=>{doubleOperatorCheck('.')}}>.</td>
-                <td onClick={()=>{calculationFunction(input);addHistoryRecord(input)}}>=</td>
+                <td onClick={()=>{calculationFunction(input)}}>=</td>
                 <td onClick={()=>{doubleOperatorCheck('/')}}>/</td>
               </tr>
               </tbody>
             </table>
           </div>
-          <div id='history-container'>
-            <h1>History</h1>
-              <div id='history-content'>
-                <History addHistoryRecord={addHistoryRecord}/>
-              </div>
-          </div> 
         </div>
     </>
   );
